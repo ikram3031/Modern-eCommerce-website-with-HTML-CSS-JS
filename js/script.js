cbbaf0d0fe32734a425e1006379ec1133c96e19f
ikram3031 <mdikram.3031@gmail.com>
2022-05-25 03:42:44 +0600

    25-05-2022

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//model variables
+
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+//modal function
+const modalCloseFunc = function () { modal.classList.add('closed')}
+
+//mopdal eventlistener
+modalCloseOverlay.addEventListener ('click' , modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+//notification toast variables
+const notificationToast = document.querySelector('[data-toast');
+const toastCloseBtn = document.querySelector('[data-toast-close');
+
+//notification toast eventlistener 
+toastCloseBtn.addEventListener('click', function(){
+    notificationToast.classList.add('closed');
+})
+
